b9f72a11b37883a73b546c25c91834a4cc3c4f67
Divyansh Yadav <101042001+Divi0007@users.noreply.github.com>
2023-08-24 11:01:17 +0530

    Delete cohort directory

--- cohort/batch/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import batches
-
-class batchfilter(django_filters.FilterSet):
-    class Meta:
-        model=batches
-        fields=['agegroup','days','status','timing','vacancy','startdate']
